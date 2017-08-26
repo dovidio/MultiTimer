7c1fa120f8cf2dfad4dc099c75538dbd3708d82a
Umberto D'Ovidio <umberto.dovidio@gmail.com>
2017-08-26 19:43:31 +0200

    refactoring based on crud database operation

--- app/src/main/java/io/dovid/multitimer/MainActivity.java
+++ app/src/main/java/io/dovid/multitimer/MainActivity.java
@@ -1,6 +1,10 @@
 package io.dovid.multitimer;
 
 import android.app.DialogFragment;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -8,29 +12,38 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity implements TimerSetupDialog.TimerSetupDialogListener {
+import io.dovid.multitimer.database.DatabaseHelper;
+import io.dovid.multitimer.model.TimerDAO;
+
+public class MainActivity extends AppCompatActivity implements CreateTimerDialog.TimerCreateDialogListener, TimerUpdateDialog.TimerUpdateDialogListener {
 
     private RecyclerView mRecyclerView;
     private TimersAdapter mAdapter;
+    private DatabaseHelper databaseHelper;
+
+    BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (mAdapter != null) {
+                mAdapter.refreshTimers();
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        databaseHelper = databaseHelper.getInstance(this);
 
         mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
-
         mAdapter = new TimersAdapter(this);
 
         RecyclerView.LayoutManager manager = new LinearLayoutManager(this);
-
-
 
         mRecyclerView.setLayoutManager(manager);
         mRecyclerView.setAdapter(mAdapter);
@@ -39,37 +52,40 @@ public class MainActivity extends AppCompatActivity implements TimerSetupDialog.
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mAdapter.addTimer();
-                mRecyclerView.setAdapter(mAdapter);
+                CreateTimerDialog createDialog = CreateTimerDialog.getInstance();
+                createDialog.show(getFragmentManager(), "createTimer");
             }
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onOkClicked(long time, int position, DialogFragment dialog) {
-        mAdapter.saveTimer(position, time);
+    public void onCreateTimer(String name, long time, DialogFragment dialog) {
+        TimerDAO.create(databaseHelper, name, time, false, true);
+        mAdapter.refreshTimers();
         dialog.dismiss();
+    }
+
+    @Override
+    public void onUpdate(String name, long defaultTime, int timerId, DialogFragment dialog) {
+        dialog.dismiss();
+        TimerDAO.updateTimer(databaseHelper, timerId, name, defaultTime, defaultTime);
+        mAdapter.refreshTimers();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(receiver, new IntentFilter(BuildConfig.UPDATE_TIMERS));
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(receiver);
+    }
+
+    @Override
+    public void onBackPressed() {
     }
 }
