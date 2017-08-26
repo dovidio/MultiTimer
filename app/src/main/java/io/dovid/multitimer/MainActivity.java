8009753ffeaaa6e1200a3cc9902cb229504cbd89
Umberto D'Ovidio <umberto.dovidio@gmail.com>
2017-08-26 20:33:04 +0200

    now user can exit app with back button

--- app/src/main/java/io/dovid/multitimer/MainActivity.java
+++ app/src/main/java/io/dovid/multitimer/MainActivity.java
@@ -87,5 +87,10 @@ public class MainActivity extends AppCompatActivity implements CreateTimerDialog
 
     @Override
     public void onBackPressed() {
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);//***Change Here***
+        startActivity(intent);
+        finish();
     }
 }
