ee310b183b13151effa5460508ca48a8fa0d2ca4
Umberto D'Ovidio <umberto.dovidio@gmail.com>
2017-08-26 20:18:06 +0200

    add ringtone

--- app/src/main/java/io/dovid/multitimer/TimesUpActivity.java
+++ app/src/main/java/io/dovid/multitimer/TimesUpActivity.java
@@ -1,6 +1,9 @@
 package io.dovid.multitimer;
 
 import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,6 +17,7 @@ import io.dovid.multitimer.utilities.TimerRunner;
 
 public class TimesUpActivity extends AppCompatActivity {
 
+    private Ringtone r;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,17 +37,33 @@ public class TimesUpActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent i = new Intent(TimesUpActivity.this, MainActivity.class);
                 startActivity(i);
+                if (r != null && r.isPlaying()) {
+                    r.stop();
+                }
             }
         });
+
+        playAlarmRingtone();
+
     }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-    }
+    private void playAlarmRingtone() {
 
-    @Override
-    protected void onResume() {
-        super.onResume();
+        Uri alert = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+        if(alert == null){
+            // alert is null, using backup
+            alert = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+            // I can't see this ever being null (as always have a default notification)
+            // but just incase
+            if(alert == null) {
+                // alert backup is null, using 2nd backup
+                alert = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
+            }
+        }
+
+        r = RingtoneManager.getRingtone(getApplicationContext(), alert);
+        r.play();
+
     }
 }
