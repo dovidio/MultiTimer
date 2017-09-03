7db7fd59935a42e1bb7a2f13f03c5f4f06a3fd08
Umberto D'Ovidio <umberto.dovidio@gmail.com>
2017-09-03 11:42:33 +0200

    add vibration based on preferences and product flavor

--- app/src/main/java/io/dovid/multitimer/ui/TimesUpActivity.java
+++ app/src/main/java/io/dovid/multitimer/ui/TimesUpActivity.java
@@ -1,14 +1,17 @@
 package io.dovid.multitimer.ui;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
-import android.preference.Preference;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -21,6 +24,7 @@ public class TimesUpActivity extends AppCompatActivity {
 
     private static final String TAG = "TIMESUPACTIVITY";
     private Ringtone r;
+    private Vibrator v;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,13 +54,44 @@ public class TimesUpActivity extends AppCompatActivity {
     }
 
     private void playAlarmRingtone() {
-        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
-        String ringtone = preferences.getString("notifications_new_message_ringtone", null);
+        if (BuildConfig.PAID) {
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+            String ringtone = preferences.getString("notifications_new_message_ringtone", null);
+            boolean vibrate = preferences.getBoolean("notifications_new_message_vibrate", false);
 
-        if (ringtone != null) {
-            Uri uri = Uri.parse(ringtone);
-            r = RingtoneManager.getRingtone(getApplicationContext(), uri);
+            if (vibrate) {
+                Log.d(TAG, "playAlarmRingtone: vibrating");
+                v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    long[] timings = {1000, 1000};
+                    int[] amplitudes = new int[]{255, 0};
+                    v.vibrate(VibrationEffect.createWaveform(timings, amplitudes, 0));
+                } else {
+                    long[] pattern = {0, 1000, 1000};
+                    v.vibrate(pattern, 0);
+                }
+            }
+
+            if (ringtone != null) {
+                Uri uri = Uri.parse(ringtone);
+                r = RingtoneManager.getRingtone(getApplicationContext(), uri);
+                r.play();
+            }
+        } else {
+            Uri alarm = RingtoneManager.getActualDefaultRingtoneUri(this, RingtoneManager.TYPE_ALARM);
+            r = RingtoneManager.getRingtone(getApplicationContext(), alarm);
             r.play();
         }
+    }
+
+    @Override
+    protected void onStop() {
+        if (r != null && r.isPlaying()) {
+            r.stop();
+        }
+        if (v != null) {
+            v.cancel();
+        }
+        super.onStop();
     }
 }
