1de7ba140eba83f507723978408beb5b400ffa5f
Umberto D'Ovidio <umberto.dovidio@gmail.com>
2017-09-09 14:22:30 +0200

    add new function used in broadcast receiver

--- app/src/main/java/io/dovid/multitimer/utilities/TimerAlarmManager.kt
+++ app/src/main/java/io/dovid/multitimer/utilities/TimerAlarmManager.kt
@@ -4,6 +4,7 @@ import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.database.sqlite.SQLiteException
 import android.os.Build
 import io.dovid.multitimer.BuildConfig
 import io.dovid.multitimer.database.DatabaseHelper
@@ -77,6 +78,43 @@ object TimerAlarmManager {
             } else {
                 alarmManager.set(AlarmManager.RTC_WAKEUP, Date().time + minExpiredTime, PendingIntent.getBroadcast(context, 1, intentAlarm, PendingIntent.FLAG_UPDATE_CURRENT))
             }
+        }
+    }
+
+    fun setupAlarms(context: Context) {
+        var databaseHelper: DatabaseHelper? = null
+
+        try {
+            databaseHelper = DatabaseHelper.getInstance(context)
+
+            val timers = TimerDAO.getTimers(databaseHelper)
+
+            var minExpiredTime = java.lang.Long.MAX_VALUE
+            var nameOfTimer: String? = null
+            for (timer in timers) {
+                if (timer.isRunning && timer.expiredTime < minExpiredTime && timer.shouldNotify()) {
+                    minExpiredTime = timer.expiredTime
+                    nameOfTimer = timer.name
+                }
+            }
+
+            if (nameOfTimer != null) {
+
+                val intentAlarm = Intent(context, AlarmReceiver::class.java)
+                intentAlarm.action = BuildConfig.TIME_IS_UP
+                intentAlarm.putExtra(BuildConfig.EXTRA_TIMER_NAME, nameOfTimer)
+
+                val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                    alarmManager.setExact(AlarmManager.RTC_WAKEUP, Date().time + minExpiredTime, PendingIntent.getBroadcast(context, 1, intentAlarm, PendingIntent.FLAG_UPDATE_CURRENT))
+                } else {
+                    alarmManager.set(AlarmManager.RTC_WAKEUP, Date().time + minExpiredTime, PendingIntent.getBroadcast(context, 1, intentAlarm, PendingIntent.FLAG_UPDATE_CURRENT))
+                }
+            }
+        } catch (e: SQLiteException) {
+            throw RuntimeException(e)
+        } finally {
+            databaseHelper?.close()
         }
     }
 }
