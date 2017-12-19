a9d147a2486427f0fc838b3c260be22adea59899
Umberto D'Ovidio <umberto.dovidio@gmail.com>
2017-12-19 23:56:12 +0100

    provides array of color ints

---
+++ app/src/main/java/io/dovid/multitimer/ui/ColorsThemePalette.java
@@ -0,0 +1,36 @@
+package io.dovid.multitimer.ui;
+
+import io.dovid.multitimer.R;
+
+/**
+ * Created by umber on 12/15/2017.
+ */
+
+public class ColorsThemePalette {
+
+    public static int[] getDarkColors() {
+        return new int[]{R.color.dark_card};
+    }
+
+    public static int[] getMaterialColors() {
+        return new int[]{R.color.material_card1,
+                R.color.material_card2,
+                R.color.material_card3,
+                R.color.material_card4,
+                R.color.material_card5,
+                R.color.material_card6,
+                R.color.material_card7,
+                R.color.material_card8,
+                R.color.material_card9,
+                R.color.material_card10};
+
+    }
+
+    public static int[] getBakeryColors() {
+        return new int[]{R.color.bakery_card1,
+                R.color.bakery_card2,
+                R.color.bakery_card3,
+                R.color.bakery_card4,
+                R.color.bakery_card5};
+    }
+}
