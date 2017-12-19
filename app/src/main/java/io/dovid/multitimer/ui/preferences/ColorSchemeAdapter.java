b610d94e395a7c415a941d452125365229c81787
Umberto D'Ovidio <umberto.dovidio@gmail.com>
2017-12-19 23:57:11 +0100

    update

--- app/src/main/java/io/dovid/multitimer/ui/preferences/ColorSchemeAdapter.java
+++ app/src/main/java/io/dovid/multitimer/ui/preferences/ColorSchemeAdapter.java
@@ -36,7 +36,7 @@ public class ColorSchemeAdapter extends RecyclerView.Adapter<ColorSchemeAdapter.
 
     @Override
     public ColorSchemeViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.color_scheme_item, null);
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.color_scheme_item, parent, false);
         return new ColorSchemeViewHolder(v);
     }
 
